4b85be81d498f52ce63be2bb40a17aeb5e34d9aa
YerlanTemirbolat <erlantemirbolat87@gmail.com>
2021-05-17 02:09:43 +0600

    feat: solutions

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,11 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    
+    NSInteger res = 0;
+    
+    for (NSInteger i = 0; i < 8; ++i) {
+        res = (res << 1) + (n >> i & 1);
+    }
+    return res;
 }
--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -2,8 +2,18 @@
 
 @implementation IPFormatConverter
 
-- (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
-}
+//- (NSString *)ipFormatConverter:(NSArray *)numbersArray {
+//
+//    NSMutableString *total = [[NSMutableString alloc] init];
+//    for (int i = 0; i < 4; i++) {
+//        if (total > 0) {
+//            total appendString: i;
+//        }
+//    }
+//
+//   
+//    return 0;
+//}
 
 @end
+
